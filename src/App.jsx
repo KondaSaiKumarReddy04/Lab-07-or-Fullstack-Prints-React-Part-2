2e170fa4d8afd3788bd2a8ffce29a18679f2aa1c
saikumarkonda <37613399-saikumarkonda@users.noreply.replit.com>
2024-12-10 04:11:00 +0000

    Updates products,orders,cart components in the App.jsx file.
    Changes made to Add.jsx where several components were added, card, cardlist components are updated. Changes made to singleview file also.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header';
 import CardList from './components/CardList';
+import Cart from './components/Cart';
 import SingleView from './components/SingleView';
 import productData from './data/full-products';
+import CartProvider from './state/CartProvider';
+import Order from './components/Orders';
 
 
 function App() {
@@ -16,10 +19,12 @@ function App() {
       <Routes>
         <Route path="/" element={<CardList />} />
         <Route path="/product/:id" element={<SingleView />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<Order />} />
       </Routes>
     </CartProvider>
     </div>
   );
 }
 
-export default App;
+export  default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
+import { CartContext } from '../state/CartProvider';
 
 const Header = () => {
-  // TODO: Update the total items to a value from the cart context
-  const totalItems = 0;
+  // Import the cart state from the CartContext
+  const { cartItems } = useContext(CartContext);
+  // Use the reduce function to calculate the total number of items in the cart
+  const totalItems = cartItems.reduce((acc, item) => acc + item.quantity, 0);
 
   return (
     <nav className="dt w-100 border-box pa3 ph5-ns">
@@ -13,7 +16,7 @@ const Header = () => {
       <div className="dtc v-mid w-75 tr">
         <Link className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" to="/" title="Products">Products</Link>
         <Link className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" to="/orders" title="/orders">Orders</Link>
-        <Link className="link dim dark-gray f6 f5-ns dib" to="/cart" title="Cart">Cart <span class="ba b--black-20 br-pill pa2">{totalItems}</span></Link>
+        <Link className="link dim dark-gray f6 f5-ns dib" to="/cart" title="Cart">Cart <span className="ba b--black-20 br-pill pa2">{totalItems}</span></Link>
       </div>
     </nav>
 
